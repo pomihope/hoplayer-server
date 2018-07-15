ee47c392eeafc0dbd6908b6025e5408cf0136833
Pin Lin <moneycat711@gmail.com>
2018-07-15 14:39:48 +0800

    Add users management

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, redirect, render_template
+from flask import Flask, Response, request, redirect, render_template
 from flask_socketio import SocketIO, send, emit
+from flask_login import LoginManager, UserMixin
 
+import json
 import time
 
 # 宣告 Flask 實體並載入設定
@@ -11,10 +13,36 @@ app.config.from_pyfile('config.py')
 
 # 套用 SocketIO 至 Flask 實體
 socket = SocketIO(app)
+# 套用 LoginManager 至 Flask 實體
+login_manager = LoginManager(app)
+
+# 儲存使用者資訊
+users = {}  
+
+class User(UserMixin):      
+    def __init__(self, userid):
+        super()
+        self.id = userid
+
+@login_manager.user_loader
+def user_loader(userid):
+    if userid in users:    
+        return User(userid)
+    else:
+        return None
 
 @app.route('/')
 def index_page():
     return redirect('/test')
+
+@app.route('/users', methods=['GET', 'POST'])
+def users_api():
+    if request.method == 'GET':
+        return Response(json.dumps({'status': 200}, ensure_ascii=False), content_type='application/json; charset=utf-8')
+    if request.method == 'POST':
+        # 取得使用者名稱
+        username = request.json['name']
+        return Response(json.dumps({'status': 201}, ensure_ascii=False), content_type='application/json; charset=utf-8'), 201
 
 @app.route('/test')
 def test_page():
